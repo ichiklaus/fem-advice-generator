8e90bd21ed9516d1da2f5e5c1e8195b85ba5f55b
Nicolás Moreira <nicola1994n@gmail.com>
2022-04-19 16:13:11 -0500

    fix breakpoints; button positioning

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,24 +6,8 @@ import {
   CardActions,
   Typography,
 } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import axios from "axios";
 import { Component } from "react";
-
-const theme = createTheme({
-  breakpoints: {
-    values: {
-      xxs: 0, // small phone
-      xs: 375, // phone
-      sm: 600, // tablets
-      smd: 800,
-      md: 900, // small laptop
-      slg: 1024,
-      lg: 1200, // desktop
-      xl: 1400, // large screens
-    },
-  },
-});
 
 export default class Card extends Component {
   state = {
@@ -64,26 +48,27 @@ export default class Card extends Component {
 
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <Container
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            width: "100%",
-            height: "100%",
-          }}
-        >
+      <Container
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <div className="card-wrapper">
           <Card
-            className=" card-layout card-bg--gb "
+            className="card-layout card-bg--gb "
             sx={{
-              width: "30rem",
-              height: "35vh",
-              // position: "relative",
               borderRadius: 4,
             }}
           >
-            <CardContent>
+            <CardContent
+              sx={{
+                height: "40%",
+              }}
+            >
               <Typography
                 className="card-tc-font card-tc--c card-tc--ng"
                 sx={{
@@ -103,54 +88,30 @@ export default class Card extends Component {
                 {`"${this.state.advice}"`}
               </Typography>
             </CardContent>
-            <div
-            className="divider-wrapper">
+            <div className="divider-wrapper">
               <i className="card-divider desktop mobile"></i>
             </div>
-            <CardActions
-              sx={{
-                position: "absolute",
-                bottom: "-10px",
-                left: "50%",
-                transform: "translate(-50%,50%)",
-              }}
-            >
-              <Button
-                sx={{
-                  position: "relative",
-                  // bottom: "260px",
-                  // xxs: 0, // small phone
-                  // xs: 375, // phone
-                  // sm: 600, // tablets
-                  // md: 900, // small laptop
-                  // lg: 1200, // desktop
-                  // xl: 1536, // large screens
-                  bottom: {
-                    xxs: "80px",
-                    xs: "185px",
-                    sm: "340px",
-                    smd: "355px",
-                    md: "280px",
-                    slg: "250px",
-                    lg: "205px",
-                    xl: "240px",
-                  },
-                  borderRadius: "50%",
-                  width: "auto",
-                  height: "60px",
-                }}
-                className="card-button--ng"
-                variant="contained"
-                color="primary"
-                size="small"
-                onClickCapture={this.refreshAdvice}
-              >
-                <i className="icon-dice"></i>
-              </Button>
-            </CardActions>
+            <div className="action-wrapper">
+              <CardActions>
+                <Button
+                  sx={{
+                    borderRadius: "50%",
+                    width: "auto",
+                    height: "60px",
+                  }}
+                  className="card-button--ng"
+                  variant="contained"
+                  color="primary"
+                  size="small"
+                  onClickCapture={this.refreshAdvice}
+                >
+                  <i className="icon-dice"></i>
+                </Button>
+              </CardActions>
+            </div>
           </Card>
-        </Container>
-      </ThemeProvider>
+        </div>
+      </Container>
     );
   }
 }
